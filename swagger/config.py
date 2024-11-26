57f0b9e8f615ec367d76c8ace756184dab22f978
Josef-TL <143087518+Josef-TL@users.noreply.github.com>
2024-11-26 14:59:19 +0100

    Create config.py

---
+++ swagger/config.py
@@ -0,0 +1,32 @@
+from flasgger import Swagger
+
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": 'apispec',
+            "route": '/apispec.json',
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/"
+}
+
+template = {
+    "info": {
+        "title": "Kong Arthurs Hotel Case API",
+        "description": "API for Kong Arthurs Hotel Case",
+        "version": "1.0.0",
+    },
+    "securityDefinitions": {
+        "Bearer": {
+            "type": "apiKey",
+            "name": "Authorization",
+            "in": "header",
+            "description": "JWT Authorization header using the Bearer scheme. Example: \"Bearer {token}\""
+        }
+    }
+}
