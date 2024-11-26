5810c8c6c8290652090286a954093e529a3e115d
Josef-TL <143087518+Josef-TL@users.noreply.github.com>
2024-11-26 16:54:11 +0100

    Update db_service.py

--- db_service.py
+++ db_service.py
@@ -6,12 +6,11 @@ load_dotenv()
 
 db_path = os.getenv('DB_PATH')
 
+
 def init():
-  ##
-  def init():
     """Initialize the guests table if it doesn't exist."""
     with sqlite3.connect(db_path) as con:
         cur = con.cursor()
         cur.execute('''
-            CREATE TABLE HERE
+        CREATE TABLE HERE
         ''')
