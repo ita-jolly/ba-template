5939378cd2b42d042a7ab64e0f1ae3495886e9d3
Josef-TL <143087518+Josef-TL@users.noreply.github.com>
2024-11-26 14:58:28 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, request, jsonify
+import db_service
+from flasgger import Swagger, swag_from
+from dotenv import load_dotenv
+from swagger.swagger_config import swagger_config
+
+load_dotenv()
+
+app = Flask(__name__)
+swagger = Swagger(app, config=swagger_config)
+
+db_service.init()
+
+
+@app.route('/')
+def index():
+  return "Welcome to API"
+
+
+# @app.route('/gettemplate', methods=['GET'])
+# @swag_from('swagger/get_template.yml')
+# def get_guests():
+#  return "got template"
+
+if __name__ == '__main__':
+    app.run()
